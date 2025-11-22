a4e3e18e1efaace737fcd4f57e8173d575b7391e
Jacob McSwain <jacob@mcswain.dev>
2025-11-22 00:00:16 -0600

    api: targets: actually use database

--- internal/server/controllers/v1/targets/targets.go
+++ internal/server/controllers/v1/targets/targets.go
@@ -3,14 +3,18 @@ package targets
 import (
 	v1APIModels "github.com/USA-RedDragon/astro-processing/internal/server/apimodels/v1"
 	"github.com/USA-RedDragon/astro-processing/internal/server/middleware"
-	"github.com/USA-RedDragon/astro-processing/internal/store/models/targetscheduler"
 	"github.com/gin-gonic/gin"
 )
 
 func ListTargets(c *gin.Context) {
-	c.MustGet(middleware.DepInjectionKey)
+	di := c.MustGet(middleware.DepInjectionKey).(*middleware.DepInjection)
+	targets, err := di.Store.ListTargets()
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
 	resp := v1APIModels.ListTargetsResponse{
-		Targets: []targetscheduler.Target{},
+		Targets: targets,
 	}
 	c.JSON(200, resp)
 }
